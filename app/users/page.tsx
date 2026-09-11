06de019672727b5ded748350fce58b719129a146
three-onefouronefive <lawasknowell51@gmail.com>
2026-09-12 01:11:07 +0800

    factored out usertable + user for ease of access

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,33 +1,13 @@
 import React from 'react'
+import UserTable from './UserTable';
 
-interface User {
-  id: number; 
-  name: string;
-  email: string;
-}
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store' });
-  const users: User[] = await res.json();
 
   return (
     <>
       <h1><b>Users</b></h1>
-      
-      <table className='table table-bordered table-xs'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody className='text-lime-950'>
-          {users.map(user => <tr key={user.id}>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-          </tr>)}
-        </tbody>
-      </table>
+      <UserTable/>
     </>
   )
 }
